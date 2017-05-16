864e452a13f171968c929e62dfadbaf74c381642
AmyCollishaw <a.collishaw@kscs.org.uk>
2017-05-16 13:00:14 +0000

    Added responsive menu module and configured the css

--- sites/all/themes/kenstimpson/template.php
+++ sites/all/themes/kenstimpson/template.php
@@ -5,3 +5,12 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * kenstimpson theme.
  */
+
+// menus customization
+function mysubthemename_menu_tree__main_menu($variables){
+   if (preg_match("/\bmenu-subnavigation\b/i", $variables['tree'])){
+    return '<ul id="menu-navigation">' . $variables['tree'] . '</ul>';
+  } else {
+    return '<ul class="menu-subnavigation">' . $variables['tree'] . '</ul>';
+  }
+}
